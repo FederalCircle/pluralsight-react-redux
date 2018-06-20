9d3cd6f677123ecd9c67c4be352c6557f5accd5e
Alyson Maia <alyson.maia@gmail.com>
2018-06-20 15:48:38 -0300

    actiontypes contants created

--- src/redux/actions/toDoActions.js
+++ src/redux/actions/toDoActions.js
@@ -1,3 +1,5 @@
+import * as types from './actionTypes'
+
 export function addToDo(toDo) {
-  return { type: 'ADD_TODO', toDo }
+  return { type: types.ADD_TODO, toDo }
 }
--- src/redux/reducers/toDoReducer.js
+++ src/redux/reducers/toDoReducer.js
@@ -1,6 +1,8 @@
+import * as types from '../actions/actionTypes'
+
 export default function toDoReducer(state = [], action) {
   switch (action.type) {
-    case 'ADD_TODO':
+    case types.ADD_TODO:
       return [...state, Object.assign({}, action.toDo)]
     default:
       return state
