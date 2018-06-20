268473fd08e8ffc54dbd140dc35cfd5fab227cd1
Alyson Maia <alyson.maia@gmail.com>
2018-06-20 18:48:28 -0300

    Mock, new actions and initial route

--- src/App.js
+++ src/App.js
@@ -1,46 +1,23 @@
 import React from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-
-import * as courseActions from './redux/actions/courseActions'
+import { Route, Switch } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+import Home from './containers/Home/Home'
+import Courses from './containers/Courses/Courses'
 
 class App extends React.Component {
-  addButtonClick = () => {
-    const title = document.getElementById('title').value
-    this.props.actions.createCourse({ title })
-  }
 
   render() {
     return (
       <div className="App">
-        <h1>Courses</h1>
-        <ul>
-          {this.props.courses.length > 0?
-            this.props.courses.map((course, i) =>
-              <li key={i}>{course.title}</li>
-            ):
-            <li>-- empty --</li>
-          }
-          {}
-        </ul>
-        <h3>Create courses</h3>
-        <input type="text" id="title" />
-        <button onClick={this.addButtonClick}>Add</button>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route path="/courses" component={ Courses } />
+          </Switch>
+        </BrowserRouter>
       </div>
     )
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    courses: state.courses
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(courseActions, dispatch)
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
